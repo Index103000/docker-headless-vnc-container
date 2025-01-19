1e86097d7de5311fec89c7543de082797022e453
root <root@localhost.localdomain>
2025-01-19 23:14:22 +0800

    新增 xephyr 用于给 PyVirtualDisplay 提供桌面显示

--- src/debian-custom/install/install_env_CloudflareBypassForScraping.sh
+++ src/debian-custom/install/install_env_CloudflareBypassForScraping.sh
@@ -2,8 +2,10 @@
 ### every exit != 0 fails the script
 set -e
 
-# Install necessary packages for Xvfb
+# Install necessary packages for Xvfb and xephyr
 # Temporarily override DEBIAN_FRONTEND only for this installation step，to avoid interactive prompts during build
+# xvfb：用于在无头环境中运行虚拟显示服务器，它不会显示任何界面，但可以作为后台服务支持图形化应用程序（如浏览器、Selenium等）。
+# xserver-xephyr：用于创建可视化的虚拟显示，它提供了一个完整的图形化界面，适用于需要显示桌面环境的应用。
 DEBIAN_FRONTEND=noninteractive apt-get update && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y \
         wget \
@@ -25,7 +27,8 @@ DEBIAN_FRONTEND=noninteractive apt-get update && \
         libvulkan1 \
         libdrm2 \
         xdg-utils \
-        xvfb
+        xvfb \
+        xserver-xephyr
 
 # Clean up apt cache to reduce image size
 apt-get clean -y
